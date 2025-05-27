8d36431ff59d8262d31356d0da16e2483b53d79a
konstantin <kostia505@gmail.com>
2025-05-27 13:25:17 +0200

    animation test

---
+++ src/components/reactComponent/TextAnimation.jsx
@@ -0,0 +1,104 @@
+// import React, { useRef, useEffect } from "react";
+// import { gsap } from "gsap";
+// import { SplitText } from "gsap/SplitText";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import { useGSAP } from "@gsap/react";
+
+// gsap.registerPlugin(SplitText, ScrollTrigger);
+
+// export default function TextAnimation({
+//   children,
+//   animateOnScroll = true,
+//   delay = 0,
+// }) {
+//   const containerRef = useRef(null);
+//   const elementRef = useRef([]);
+//   const splitRef = useRef([]);
+//   const lines = useRef([]);
+
+//   // console.log(containerRef.current);
+//   useGSAP(
+//     () => {
+//       if (!containerRef.current) {
+//         return;
+//       }
+
+//       splitRef.current = [];
+//       elementRef.current = [];
+//       lines.current = [];
+
+//       let elements = [];
+
+//       if (containerRef.current.hasAttribute("data-copy-wrapper")) {
+//         elements = Array.from(containerRef.current.children);
+//       } else {
+//         elements = [containerRef.current];
+//       }
+
+//       elements.forEach((element) => {
+//         elementRef.current.push(element);
+
+//         const split = SplitText.create(element, {
+//           type: "lines",
+//           mask: "lines",
+//           linesClass: "text-anim++",
+//         });
+
+//         splitRef.current.push(split);
+
+//         const computedStyle = window.getComputedStyle(element);
+//         const textIndent = computedStyle.textIndent;
+
+//         if (textIndent && textIndent === "0px") {
+//           if (split.lines.length > 0) {
+//             split.lines[0].style.paddingLeft = textIndent;
+//           }
+//           element.style.textIndent = "0";
+//         }
+
+//         lines.current.push(...split.lines);
+//       });
+
+//       gsap.set(lines.current, { y: "100%" });
+
+//       const animationProps = {
+//         y: "0%",
+//         duration: 1,
+//         stagger: 0.1,
+//         ease: "power4.out",
+//         delay: delay,
+//       };
+
+//       if (animateOnScroll) {
+//         gsap.to(lines.current, {
+//           ...animationProps,
+//           scrollTrigger: {
+//             trigger: containerRef.current,
+//             start: "top 95%",
+//             once: true,
+//           },
+//         });
+//       } else {
+//         gsap.to(lines.current, animationProps);
+//       }
+
+//       return () => {
+//         splitRef.current.forEach((split) => {
+//           if (split) {
+//             split.revert();
+//           }
+//         });
+//       };
+//     },
+//     {
+//       scope: containerRef,
+//       dependencies: [animateOnScroll, delay],
+//     },
+//   );
+
+//   return (
+//     <div ref={containerRef} data-copy-wrapper="true">
+//       {children}
+//     </div>
+//   );
+// }
